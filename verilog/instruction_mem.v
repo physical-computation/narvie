e221c5c5b973b2826a19857c6f92b46a10837d58
Ryan Voo <ryanvoo95@gmail.com>
2018-10-14 15:18:30 +0100

    Resized instruction mem

--- verilog/instruction_mem.v
+++ verilog/instruction_mem.v
@@ -1,13 +1,13 @@
 //RISC-V instruction memory
 module instruction_memory(addr, out);
 	input[31:0]	addr;
-	output[31:0] out;
+	output reg[31:0] out;
 	
-	reg[31:0] instruction_memory[0:2**13-1];
+	reg[31:0] instruction_memory[0:2**10-1];
 	
 	initial begin
 		//read from "program.hex" and store the instructions in instruction memory
-		$readmemh("/home/ryan/RV32I_iCE40/verilog/program.hex",instruction_memory);
+		$readmemh("verilog/program.hex",instruction_memory);
 	end
 	
 	assign out = instruction_memory[addr >> 2];

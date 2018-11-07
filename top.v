6daf92400fd8a1f4b72d5541109c97e8527324e6
Ryan Voo <ryanvoo95@gmail.com>
2018-11-07 15:39:57 +0000

    Added initial support for UART

--- top.v
+++ top.v
@@ -1,19 +1,38 @@
 //top.v
 //Top level entity, linking cpu with data and instruction memory
 
-module top(led);
+module top(led, tx);
 	output[7:0] led;
+	output tx;
+	
+	reg[7:0] tx_data;
+	reg tx_start;
+	wire tx_ready;
 	
 	//input	clk;
-	wire clk;
+	wire clk48;
 	reg ENCLKHF = 1'b1; //clock enable
 	reg CLKHF_POWERUP = 1'b1; //power up the HFOSC circuit
 
-	SB_HFOSC #(.CLKHF_DIV("0b11")) OSCInst0 (
+	SB_HFOSC #(.CLKHF_DIV("0b00")) OSCInst0 (
 		.CLKHFEN(ENCLKHF),
 		.CLKHFPU(CLKHF_POWERUP),
-		.CLKHF(clk)
+		.CLKHF(clk48)
 	);
+	
+	reg clk24 = 0;
+	reg clk12 = 0;
+	reg clk6 = 0;
+	
+	always @(posedge clk48) begin
+		clk24 <= ~clk24;
+	end
+	always @(posedge clk24) begin
+		clk12 <= ~clk12;
+	end
+	always @(posedge clk12) begin
+		clk6 <= ~clk6;
+	end
 	
 	//Interface
 	wire[31:0] inst_in;
@@ -26,7 +45,7 @@ module top(led);
 	wire[3:0] data_sign_mask;
 	
 	cpu processor( 
-			.clk(clk), 
+			.clk(clk_proc), 
 			.inst_mem_in(inst_in), 
 			.inst_mem_out(inst_out), 
 			.data_mem_out(data_out), 
@@ -43,7 +62,7 @@ module top(led);
 		);
 	
 	data_memory data_mem(
-			.clk(clk),
+			.clk(clk_proc),
 			.addr(data_addr),
 			.write_data(data_WrData),
 			.memwrite(data_memwrite), 
@@ -52,5 +71,26 @@ module top(led);
 			.sign_mask(data_sign_mask),
 			.led(led)
 		);
+	
+	uart_tx #(.BAUDRATE(`B115200)) TX0 (
+			.clk(clk12),
+			.rstn(1'b1),
+			.data(tx_data),
+			.start(tx_start),
+			.tx(tx),
+			.ready(tx_ready)
+		);
+	
+	assign clk_proc = (tx_start) ? 1'b1 : clk6;
+	
+	always @(posedge clk6) begin
+		if(data_memwrite == 1'b1 && data_addr == 32'h2000) begin
+			tx_data <= data_WrData[7:0] + "0";
+			tx_start <= 1'b1;
+		end
+		if(tx_ready == 1'b0 && tx_start == 1'b1) begin
+			tx_start <= 1'b0;
+		end
+	end
 
 endmodule

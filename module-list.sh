4f03df52aef6e95fa369d679c6c816f3add2a2a9
Harry Sarson <harry.sarson@hotmail.co.uk>
2019-03-18 11:43:20 +0000

    Move narvie verilog files into subdirectory

--- module-list.sh
+++ module-list.sh
@@ -4,15 +4,18 @@ set -e
 
 export UART_RX=../peripherals/uart-rx
 export UART_TX=../peripherals/uart-tx
+export NARVIE_VERILOG=../narvie-verilog
 
 export UART_MODULES="$UART_TX/baudgen_tx.v $UART_TX/uart_tx.v \
     $UART_RX/baudgen_rx.v $UART_RX/uart_rx.v"
 
 export COMMON_MODULES="verilog/mux2to1.v verilog/alu_control.v verilog/pipeline_registers.v \
-    verilog/alu.v ../data_memory_iCE40UP5K.v verilog/program_counter.v verilog/branch_decide.v \
+    verilog/alu.v verilog/program_counter.v verilog/branch_decide.v \
     verilog/forwarding_unit.v verilog/branch_predictor.v verilog/imm_gen.v verilog/control_unit.v \
     verilog/adder.v verilog/CSR_iCE40UP5K.v verilog/dataMem_mask_gen.v"
 
-export NARVIE_MODULES="../cpu.v ../rx_instruction.v ../main.v ../uart_regfile.v"
+export NARVIE_MODULES="$NARVIE_VERILOG/cpu.v $NARVIE_VERILOG/rx_instruction.v \
+    $NARVIE_VERILOG/main.v $NARVIE_VERILOG/uart_regfile.v \
+    $NARVIE_VERILOG/data_memory_iCE40UP5K.v"
 
 export MODULES="$COMMON_MODULES $UART_MODULES $NARVIE_MODULES"
